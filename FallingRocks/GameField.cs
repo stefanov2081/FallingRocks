319e226b835e731e54556c1cbd5727bbcf5c2857
Vladimir Stefanov <Vladimir Stefanov@VLADIMIRSTEFANO>
2018-01-18 17:43:55 +0200

    Test game field size - p

---
+++ FallingRocks/GameField.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FallingRocks
+{
+    public class GameField
+    {
+        private int width;
+        private int height;
+
+        public GameField(int width, int height)
+        {
+            Width = width;
+            this.height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Width must be greater than 0.");
+                }
+
+                width = value;
+            }
+        }
+    }
+}
