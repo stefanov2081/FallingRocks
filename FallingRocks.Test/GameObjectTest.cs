c60aff1a5d1da79da815739fc6c80b6aba63da13
Vladimir Stefanov <Vladimir Stefanov@VLADIMIRSTEFANO>
2018-02-19 14:04:53 +0200

    GameObject tests

---
+++ FallingRocks.Test/GameObjectTest.cs
@@ -0,0 +1,51 @@
+﻿namespace FallingRocks.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class GameObjectTest
+    {
+        [TestMethod]
+        public void Constructor_WithZeroX_SetsX()
+        {
+            var x = 0;
+            var y = 0;
+
+            var gameObject = new GameObject(x, y);
+
+            Assert.AreEqual(x, gameObject.X, "GameObject.X is not set.");
+        }
+
+        [TestMethod]
+        public void Constructor_WithZeroY_SetsY()
+        {
+            var x = 0;
+            var y = 0;
+
+            var gameObject = new GameObject(x, y);
+
+            Assert.AreEqual(y, gameObject.Y, "GameObject.Y is not set.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "GameObject.X must be equal or greater than 0. ArgumentException must be thrown otherwise.")]
+        public void Constructor_WithNegativeX_ThrowsArgumentException()
+        {
+            var x = -1;
+            var y = 0;
+
+            var gameObject = new GameObject(x, y);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "GameObject.Y must be equal or greater than 0. ArgumentException must be thrown otherwise.")]
+        public void Constructor_WithNegativeY_ThrowsArgumentException()
+        {
+            var x = 0;
+            var y = -1;
+
+            var gameObject = new GameObject(x, y);
+        }
+    }
+}
---
+++ FallingRocks/GameObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FallingRocks
+{
+    public class GameObject
+    {
+        private int x;
+        private int y;
+
+        public GameObject(int x, int y)
+        {
+            X = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+        }
+    }
+}
